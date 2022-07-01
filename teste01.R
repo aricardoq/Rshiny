b62f7b6dd773bca61a948746d2344d820a76048c
André Raqjr <aricardo.q@edu.unirio.br>
2022-07-01 13:58:23 -0300

    first

---
+++ teste01.R
@@ -0,0 +1,7 @@
+install.packages("shiny")
+install.packages("rtools")
+library(shiny)
+# Launches an app, with the app's source code included
+runExample("06_tabsets")
+# Lists more prepackaged examples
+runExample()
